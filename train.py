56f47a33dc87421938d8ebc3284fa3eaeb91fd9b
Bianca Matos de Barros <biancamatos.ads@gmail.com>
2018-05-15 21:59:48 -0300

    Testing

---
+++ train.py
@@ -0,0 +1,7 @@
+# import comet_ml in the top of your file
+from comet_ml import Experiment
+
+# Add the following code anywhere in your machine learning file
+experiment = Experiment(api_key="ZYZWOp8YGYbWZjtv5b0kJJqC1")
+
+# Run your code and go to https://www.comet.ml
