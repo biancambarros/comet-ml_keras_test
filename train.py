68872d542299ecdaf21b73119cd1bf5951d2df49
Bianca Matos de Barros <biancamatos.ads@gmail.com>
2018-05-15 23:14:38 -0300

    Testing experiment class

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ from comet_ml import Experiment
 experiment = Experiment(api_key="ZYZWOp8YGYbWZjtv5b0kJJqC1") #creating a new Experiment object, sending the APY key obtained from comet.ml
 
 # Run your code and go to https://www.comet.ml - creation of experiment
-#Adding attributes
-experiment.project_name('Testing machine learning with keras in comet.ml')
+# Adding attributes
+# experiment.project_name('Testing machine learning with keras in comet.ml')
 experiment.team_name('Machine learning - UFRGS CLN')
 
 #testing:
@@ -14,3 +14,5 @@ experiment.log_html('<a href="www.comet.ml"> I love Comet.ml </a>')
 
 #Documentation - Experiment class
 ##Experiment is a unit of measurable research that defines a single run with some data/parameters/code/results.
+
+
